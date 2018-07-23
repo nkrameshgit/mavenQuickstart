244492b2fde91f7700a2b74cee5deee05f2611b8
rn85006 <rn85006@USTX15369L01689.nam.nsroot.net>
2018-07-23 12:05:50 -0500

    one more commit

--- java/src/main/java/com/maven/quickstart/java/HelloWorld.java
+++ java/src/main/java/com/maven/quickstart/java/HelloWorld.java
@@ -4,10 +4,11 @@ package com.maven.quickstart.java;
  * Hello world!
  *
  */
-public class App 
+public class HelloWorld 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );;
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
